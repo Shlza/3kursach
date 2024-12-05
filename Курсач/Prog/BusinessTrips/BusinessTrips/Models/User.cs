86b972a405580b65faa75b15ac6bb2c63aafbbca
Shlza <86535231+Shlza@users.noreply.github.com>
2024-12-05 04:12:27 +0300

    Clear
    old files deleted

--- Курсач/Prog/BusinessTrips/BusinessTrips/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿public class User
-{
-    public int UserID { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Role { get; set; } // "Admin", "Employee"
-}
